d5ccc374de866cef552ec826b921e077fd716e72
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-22 22:18:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+
+
+
+
+
+
+
+
+
+
+
+
+#def badge_maker(name)
+  #"hello, my names is #{name}!"
+#end
+  
+#def batch_badge_creator(array)
+  #array = [edsger, ada, charles, alan ,grace, linus, matz]
