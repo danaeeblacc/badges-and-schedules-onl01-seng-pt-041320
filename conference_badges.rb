170e85aa1fcc2342a8990c890b3efb1ef8d1c965
Teinelle Charles <teinelledcharles@gmail.com>
2020-04-22 22:37:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,13 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+
+def batch_badge_creator(speakers)
+  badge_messages = []
+  speakers.each do |speaker|
+    message = badge_maker(speaker)
+    badge_messages << message
+  end
+  badge_messages
+end
